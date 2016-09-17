8621c714849ebe0efbf810cfecf27c97350259e8
Bobby Smith <bobbysmith24@gmail.com>
2016-09-17 17:44:52 -0500

    refactor

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const row = document.querySelector('.row');
 const select = document.querySelector('.select');
 const container = document.querySelector('.container');
-let digit;
+const rulesIcon = [[1,1,1], [1,1,0], [1,0,1], [1,0,0], [0,1,1], [0,1,0], [0,0,1], [0,0,0]];
+let ruleNumber;
 
 // Creates options for dropdown 0 - 255
 for (let i = 0; i < 256; i++) {
@@ -13,24 +14,27 @@ for (let i = 0; i < 256; i++) {
 
 // Renders automata to screen
 function generateAutomata() {
-  container.innerHTML = '';
+  ruleNumber = select.options[select.selectedIndex].value;
 
-  let row = document.createElement('div');
-  row.setAttribute('class', 'row');
-  container.appendChild(row);
+  if (parseInt(ruleNumber) >= 0) {
+    container.innerHTML = '';
 
-  // Creates divs and appends to row based on width of window
-  for(let i = 1; i < container.clientWidth / 4; i++) {
-    let div = document.createElement('div');
-    row.appendChild(div);
+    // Create oringial row and append it to the container
+    let row = document.createElement('div');
+    row.setAttribute('class', 'row');
+    container.appendChild(row);
+
+    // Creates divs and appends to row based on width of window
+    for(let i = 1; i < container.clientWidth / 4; i++) {
+      let div = document.createElement('div');
+      row.appendChild(div);
+    }
+
+    randomizeRow(row);
+    for (let i = 1; i < container.clientHeight / 4; i++) {
+      setTimeout(duplicateRow, i * 10);
+    }
   }
-
-  digit = select.options[select.selectedIndex].value;
-  randomizeRow(row);
-  for (let i = 1; i < container.clientHeight / 4; i++) {
-    setTimeout(duplicateRow, i * 10);
-  }
-
 }
 
 // Gets selcted value from dropdown
@@ -38,8 +42,8 @@ function getSelectValue() {
   return select.options[select.selectedIndex].value;
 }
 
-// Converts number to booleans based on binary
-function numberToBinaryArray(num) {
+// Converts number to array of booleans based on on each bit in the number
+function numbertoBooleanArray(num) {
   let binary = Number(num).toString(2);
   let arr = [];
   while(binary.length < 8) {
@@ -77,23 +81,18 @@ function processRow(row, parentRow) {
   for (let i = 0; i < row.childNodes.length; i++) {
     let target = row.childNodes[i];
     let parent = parentRow.childNodes[i];
-    let leftSibling = parent.previousElementSibling || parentRow.childNodes[parentRow.childNodes.length - 1];
-    let rightSibling = parent.nextElementSibling || parentRow.childNodes[0];
-    let toggleClass = setActiveIfMatchesRule.bind(null, target, leftSibling, parent, rightSibling);
+    let left = parent.previousElementSibling || parentRow.childNodes[parentRow.childNodes.length - 1];
+    let right = parent.nextElementSibling || parentRow.childNodes[0];
+    let toggleClass = setActiveIfMatchesRule.bind(null, target, left, parent, right);
 
-    toggleClass([1,1,1], numberToBinaryArray(digit)[0]);
-    toggleClass([1,1,0], numberToBinaryArray(digit)[1]);
-    toggleClass([1,0,1], numberToBinaryArray(digit)[2]);
-    toggleClass([1,0,0], numberToBinaryArray(digit)[3]);
-    toggleClass([0,1,1], numberToBinaryArray(digit)[4]);
-    toggleClass([0,1,0], numberToBinaryArray(digit)[5]);
-    toggleClass([0,0,1], numberToBinaryArray(digit)[6]);
-    toggleClass([0,0,0], numberToBinaryArray(digit)[7]);
+    for (let j = 0; j < rulesIcon.length; j++) {
+      toggleClass(rulesIcon[j], numbertoBooleanArray(ruleNumber)[j]);
+    }
   }
 }
 
-function setActiveIfMatchesRule(target, leftSibling, parent, rightSibling, rule, ruleValue) {
-  let matchesRule = state(leftSibling) === rule[0] && state(parent) === rule[1] && state(rightSibling) === rule[2];
+function setActiveIfMatchesRule(target, left, parent, right, rule, ruleValue) {
+  let matchesRule = state(left) === rule[0] && state(parent) === rule[1] && state(right) === rule[2];
 
   if(matchesRule) {
     toggleActive(target, ruleValue);
